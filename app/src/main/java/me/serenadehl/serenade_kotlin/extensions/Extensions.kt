0607bde5cc649030d4dc8d542327628c40f99b3c
Serenade <SerenadeHL@163.com>
2018-07-16 15:19:33 +0800

    添加Extensions

--- app/src/main/java/me/serenadehl/serenade_kotlin/extensions/Extensions.kt
+++ app/src/main/java/me/serenadehl/serenade_kotlin/extensions/Extensions.kt
@@ -1,14 +1,18 @@
 package me.serenadehl.serenade_kotlin.extensions
 
 import android.app.Activity
-import android.app.PendingIntent.getActivity
+import android.app.ActivityManager
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import me.serenadehl.serenade_kotlin.BuildConfig
+import me.serenadehl.serenade_kotlin.utils.app.AppManager
 import me.serenadehl.serenade_kotlin.utils.sharedpre.SPUtil
 import android.app.Fragment as AppFragment
 import android.support.v4.app.Fragment as SupportFragment
@@ -76,3 +80,87 @@ inline fun Any.log() {
  * 保存到SP
  */
 inline fun <T> T.saveToSP(key: String) = apply { SPUtil.putString(key, toString()) }
+
+/**
+ * 获取屏幕宽度,包含NavigationBar
+ */
+inline fun Activity.getScreenWidth(): Int {
+    var dpi = 0
+    val display = windowManager.defaultDisplay
+    val dm = DisplayMetrics()
+    val c: Class<*>
+    try {
+        c = Class.forName("android.view.Display")
+        val method = c.getMethod("getRealMetrics", DisplayMetrics::class.java)
+        method.invoke(display, dm)
+        dpi = dm.widthPixels
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return dpi
+}
+
+/**
+ * 获取屏幕高度,包含NavigationBar
+ */
+inline fun Activity.getScreenHeight(): Int {
+    var dpi = 0
+    val display = windowManager.defaultDisplay
+    val dm = DisplayMetrics()
+    val c: Class<*>
+    try {
+        c = Class.forName("android.view.Display")
+        val method = c.getMethod("getRealMetrics", DisplayMetrics::class.java)
+        method.invoke(display, dm)
+        dpi = dm.heightPixels
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return dpi
+}
+
+/**
+ * 判断APP是否在前台
+ */
+inline fun Context.isRunningForeground(packageName: String): Boolean {
+    val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    val cn = am.getRunningTasks(1)[0].topActivity
+    val currentPackageName = cn.packageName
+    return currentPackageName != null && currentPackageName == packageName
+}
+
+/**
+ * 获取版本号
+ */
+inline fun Context.getVersionName(): String {
+    // getPackageName()是你当前类的包名，0代表是获取版本信息
+    val packInfo = packageManager.getPackageInfo(packageName, 0)
+    return packInfo.versionName
+}
+
+/**
+ * 上一次点击退出按钮的时间
+ */
+private var mLastBackPressedTime: Long = 0
+
+/**
+ * 双击返回退出App
+ */
+fun Context.exitApp() {
+    if (System.currentTimeMillis() - mLastBackPressedTime > 2000) {
+        toast("再按一次退出程序")
+        mLastBackPressedTime = System.currentTimeMillis()
+    } else {
+        AppManager.instance.exitApp()
+    }
+}
+
+/**
+ * 拨打电话
+ */
+inline fun Context.callPhone(phone: String) = startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone")))
+
+/**
+ * 发送短信
+ */
+inline fun Context.sendMessage(phone: String) = startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("smsto:$phone")))
--- app/src/main/java/me/serenadehl/serenade_kotlin/ui/MainActivity.kt
+++ app/src/main/java/me/serenadehl/serenade_kotlin/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package me.serenadehl.serenade_kotlin.ui
 
 import me.serenadehl.serenade_kotlin.R
 import me.serenadehl.serenade_kotlin.base.mvpbase.*
+import me.serenadehl.serenade_kotlin.extensions.callPhone
+import me.serenadehl.serenade_kotlin.extensions.sendMessage
 
 class MainActivity : MVPBaseActivity<MainPresenter>(), IMainView {
     override fun createPresenter() = MainPresenter()
@@ -9,6 +11,7 @@ class MainActivity : MVPBaseActivity<MainPresenter>(), IMainView {
     override fun layout() = R.layout.activity_main
 
     override fun onActivityCreated() {
-        mPresenter.loginByPhone("17600697395", "Haoliang0423")
+//        mPresenter.loginByPhone("17600697395", "Haoliang0423")
+        callPhone(17600697395.toString())
     }
 }
